9703410497dd7998e415ee63ca804f6a03ac30e1
Cenk Senyuz <cenksenyuz@gmail.com>
2019-01-09 09:22:34 +0300

    Initial Commit

---
+++ PlayingCard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PlayingCard
+//
+//  Created by Cenk on 21.12.2018.
+//  Copyright © 2018 cenksenyuz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    var deck = PlayingCardDeck()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
+}
+
