b4e36c478244e1f889067239caaff573d44296fc
Cenk Senyuz <cenksenyuz@gmail.com>
2019-01-11 13:57:49 +0300

    add @IBDesignable and @IBInspectible to stored properties, add drawPips() function

--- PlayingCard/PlayingCardView.swift
+++ PlayingCard/PlayingCardView.swift
@@ -8,20 +8,24 @@
 
 import UIKit
 
+@IBDesignable
 class PlayingCardView: UIView {
-    var rank: Int = 5 {
+    @IBInspectable
+    var rank: Int = 12 {
         didSet {
             setNeedsDisplay()
             setNeedsLayout()
         }
     }
+    @IBInspectable
     var suit: String = "❤️"{
         didSet {
             setNeedsDisplay()
             setNeedsLayout()
         }
     }
-    var isFaceUp:Bool = true{
+    @IBInspectable
+    var isFaceUp:Bool = true {
         didSet {
             setNeedsDisplay()
             setNeedsLayout()
@@ -75,6 +79,46 @@ class PlayingCardView: UIView {
             .offseetBy(dx: -lowerRightCornerLabel.frame.size.width, dy: -lowerRightCornerLabel.frame.size.height)
     }
     
+    private func drawPips()
+    {
+        let pipsPerRowForRank = [[0],[1],[1,1],[1,1,1],[2,2],[2,1,2],[2,2,2],[2,1,2,2],[2,2,2,2],[2,2,1,2,2],[2,2,2,2,2]]
+        
+        func createPipString(thatFits pipRect: CGRect) -> NSAttributedString {
+            let maxVerticalPipCount = CGFloat(pipsPerRowForRank.reduce(0) { max($1.count, $0) })
+            let maxHorizontalPipCount = CGFloat(pipsPerRowForRank.reduce(0) { max($1.max() ?? 0, $0) })
+            let verticalPipRowSpacing = pipRect.size.height / maxVerticalPipCount
+            let attemptedPipString = centeredAttributedString(suit, fontSize: verticalPipRowSpacing)
+            let probablyOkayPipStringFontSize = verticalPipRowSpacing / (attemptedPipString.size().height / verticalPipRowSpacing)
+            let probablyOkayPipString = centeredAttributedString(suit, fontSize: probablyOkayPipStringFontSize)
+            if probablyOkayPipString.size().width > pipRect.size.width / maxHorizontalPipCount {
+                return centeredAttributedString(suit, fontSize: probablyOkayPipStringFontSize / (probablyOkayPipString.size().width / (pipRect.size.width / maxHorizontalPipCount)))
+            } else {
+                return probablyOkayPipString
+            }
+        }
+        
+        if pipsPerRowForRank.indices.contains(rank) {
+            let pipsPerRow = pipsPerRowForRank[rank]
+            var pipRect = bounds.insetBy(dx: cornerOffset, dy: cornerOffset).insetBy(dx: cornerString.size().width, dy: cornerString.size().height / 2)
+            let pipString = createPipString(thatFits: pipRect)
+            let pipRowSpacing = pipRect.size.height / CGFloat(pipsPerRow.count)
+            pipRect.size.height = pipString.size().height
+            pipRect.origin.y += (pipRowSpacing - pipRect.size.height) / 2
+            for pipCount in pipsPerRow {
+                switch pipCount {
+                case 1:
+                    pipString.draw(in: pipRect)
+                case 2:
+                    pipString.draw(in: pipRect.leftHalf)
+                    pipString.draw(in: pipRect.rightHalf)
+                default:
+                    break
+                }
+                pipRect.origin.y += pipRowSpacing
+            }
+        }
+    }
+    
     override func draw(_ rect: CGRect) {
         // setNeedDisplay() calls this function by the system.. dont call draw(rect:) by yourself..
         let roundedRect = UIBezierPath(roundedRect: bounds, cornerRadius: cornerRadius)
@@ -82,6 +126,18 @@ class PlayingCardView: UIView {
         UIColor.white.setFill()
         roundedRect.fill()
         
+        if isFaceUp{
+            if let faceCardImage = UIImage(named: rankString+suit, in:Bundle(for: self.classForCoder), compatibleWith: traitCollection) {
+                faceCardImage.draw(in: bounds.zoom(by: SizeRatio.faceCardImageSizeToBoundsSize))
+            } else {
+                drawPips()
+            }
+        } else {
+            if let cardBackImage = UIImage(named: "cardback") {
+                cardBackImage.draw(in: bounds)
+            }
+            
+        }
     }
     
 }
@@ -127,7 +183,7 @@ extension CGRect {
     func sized (to size:CGSize) -> CGRect{
         return CGRect(origin: origin, size: size)
     }
-    func scale (by scale:CGFloat) -> CGRect {
+    func zoom (by scale:CGFloat) -> CGRect {
         let newWidth = width * scale
         let newHeight = height * scale
         return insetBy(dx:( width-newWidth) / 2, dy: (height-newHeight) / 2)
